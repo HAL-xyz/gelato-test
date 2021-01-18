ee45100ec0bdd3545687c65af9c13bab55e2fb92
Hilmar X <x@hilmarx.com>
2021-01-18 16:37:13 +0100

    fixed submit and create

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,9 +14,7 @@ import Web3Modal from 'web3modal';
 import Notify from 'bnc-notify';
 
 // Constant
-import {
-  CHI_ADDRESS,
-} from '@/helpers/Constants';
+import { CHI_ADDRESS } from '@/helpers/Constants';
 import CHI_ABI from '@/abis/chiAbi';
 
 // Helpers
@@ -34,12 +32,13 @@ import withdrawFromProxy from '@/lib/withdrawFromProxy';
 // Components
 import ShowTasks from '@/components/ShowTasks/ShowTasks';
 
-const notify = async (web3) => Notify({
-  dappId: process.env.NEXT_PUBLIC_BLOCKNATIVE_API,
-  networkId: await web3.eth.net.getId(),
-  darkMode: true,
-  desktopPosition: 'topRight',
-});
+const notify = async (web3) =>
+  Notify({
+    dappId: process.env.NEXT_PUBLIC_BLOCKNATIVE_API,
+    networkId: await web3.eth.net.getId(),
+    darkMode: true,
+    desktopPosition: 'topRight',
+  });
 
 const Home = () => {
   const { watch, register, setValue } = useForm();
@@ -98,7 +97,10 @@ const Home = () => {
     }
 
     (async () => {
-      const ethGelatoBalance = await getBalanceProxyForUser(walletAddress, web3);
+      const ethGelatoBalance = await getBalanceProxyForUser(
+        walletAddress,
+        web3
+      );
       const approxEthBalance = web3.utils.fromWei(ethGelatoBalance, 'ether');
       setGelatoBalance(approxEthBalance);
     })();
@@ -141,7 +143,11 @@ const Home = () => {
     (async () => {
       try {
         setPendingTx(true);
-        const { hash, promiEvent } = await depositToProxy(depositAmount, walletAddress, web3);
+        const { hash, promiEvent } = await depositToProxy(
+          depositAmount,
+          walletAddress,
+          web3
+        );
         (await notify(web3)).hash(hash);
 
         promiEvent.once('receipt', () => {
@@ -172,7 +178,11 @@ const Home = () => {
     (async () => {
       try {
         setPendingTx(true);
-        const { hash, promiEvent } = await withdrawFromProxy(withdrawAmount, walletAddress, web3);
+        const { hash, promiEvent } = await withdrawFromProxy(
+          withdrawAmount,
+          walletAddress,
+          web3
+        );
         (await notify(web3)).hash(hash);
 
         promiEvent.once('receipt', () => {
@@ -198,7 +208,12 @@ const Home = () => {
       (async () => {
         try {
           setPendingTx(true);
-          const { hash, promiEvent } = await submitTask(chiTokenAmount, gasThreshold, walletAddress, web3);
+          const { hash, promiEvent } = await submitTask(
+            chiTokenAmount,
+            gasThreshold,
+            walletAddress,
+            web3
+          );
           (await notify(web3)).hash(hash);
 
           promiEvent.once('receipt', async () => {
@@ -239,8 +254,11 @@ const Home = () => {
     getWalletBalance();
 
     (async () => {
-      const { cpk } = await getProxyForUser(walletAddress, web3);
-      setGelatoAccount(cpk.address);
+      const { cpk, proxyIsDeployed } = await getProxyForUser(
+        walletAddress,
+        web3
+      );
+      if (proxyIsDeployed) setGelatoAccount(cpk.address);
     })();
   }, [walletAddress]);
 
@@ -258,20 +276,15 @@ const Home = () => {
 
   return (
     <>
-      <div className="container mx-auto px-8">
-        <h1 className="font-serif text-2xl mt-8">
-          🚧 Gelato Integration Demo
-        </h1>
-        <h2 className="text-lg">
-          Please, use a Rinkeby account 😉
-        </h2>
-        {currentGasValue
-          && (
-            <h2 className="text-lg">
-              {`Current Gas Price is ${currentGasValue}`}
-            </h2>
-          )}
-        <div className="mt-8">
+      <div className='container mx-auto px-8'>
+        <h1 className='font-serif text-2xl mt-8'>🚧 Gelato Integration Demo</h1>
+        <h2 className='text-lg'>Please, use a Rinkeby account 😉</h2>
+        {currentGasValue && (
+          <h2 className='text-lg'>
+            {`Current Gas Price is ${currentGasValue}`}
+          </h2>
+        )}
+        <div className='mt-8'>
           <button
             className={`
               ${gelatoAccount ? 'bg-gray-300' : 'bg-purple-600'}
@@ -279,34 +292,31 @@ const Home = () => {
               text-white
               p-4
             `}
-            type="button"
+            type='button'
             onClick={connectWallet}
             disabled={walletAddress ? true : false}
           >
             {walletAddress ? 'Wallet Connected' : 'Connect your wallet'}
           </button>
         </div>
-        {walletAddress
-          && (
-            <>
-              <div className="mt-2">
-                <p>{`WALLET ADDRESS ${walletAddress}`}</p>
+        {walletAddress && (
+          <>
+            <div className='mt-2'>
+              <p>{`WALLET ADDRESS ${walletAddress}`}</p>
+            </div>
+            {ethBalance && (
+              <div className='mt-2'>
+                <p>{`ETH ${ethBalance}`}</p>
               </div>
-              {ethBalance
-                && (
-                  <div className="mt-2">
-                    <p>{`ETH ${ethBalance}`}</p>
-                  </div>
-                )}
-              {chiBalance
-                && (
-                  <div className="mt-2">
-                    <p>{`CHI ${chiBalance}`}</p>
-                  </div>
-                )}
-            </>
-          )}
-        <div className="mt-8">
+            )}
+            {chiBalance && (
+              <div className='mt-2'>
+                <p>{`CHI ${chiBalance}`}</p>
+              </div>
+            )}
+          </>
+        )}
+        <div className='mt-8'>
           <button
             className={`
               ${gelatoAccount ? 'bg-gray-300' : 'bg-purple-600'}
@@ -314,138 +324,158 @@ const Home = () => {
               text-white
               p-4
             `}
-            type="button"
+            type='button'
             onClick={createGelatoAccount}
             disabled={gelatoAccount ? true : false}
           >
             {gelatoAccount ? 'Gelato Account Ready' : 'Create a Gelato Account'}
           </button>
         </div>
-        {gelatoAccount
-          && (
-            <>
-              <div className="mt-2">
-                <p>{`Your Gelato Proxy address is ${gelatoAccount}`}</p>
+        {gelatoAccount && (
+          <>
+            <div className='mt-2'>
+              <p>{`Your Gelato Proxy address is ${gelatoAccount}`}</p>
+            </div>
+            <div className='mt-2'>
+              <p>{`Proxy Balance ${gelatoBalance}`}</p>
+            </div>
+          </>
+        )}
+        {gelatoAccount && (
+          <>
+            <div className='mt-8'>
+              <div className='grid grid-cols-2 gap-4 mb-4'>
+                <input
+                  defaultValue='0'
+                  min='0'
+                  name='depositAmount'
+                  type='number'
+                  ref={register}
+                />
+                <input
+                  defaultValue='0'
+                  min='0'
+                  name='withdrawAmount'
+                  type='number'
+                  ref={register}
+                />
               </div>
-              <div className="mt-2">
-                <p>{`Proxy Balance ${gelatoBalance}`}</p>
+              <div className='grid grid-cols-2 gap-4'>
+                <button
+                  className={`
+                      ${
+                        !gelatoAccount || pendingTx
+                          ? 'bg-gray-300'
+                          : 'bg-purple-600'
+                      }
+                      ${
+                        !gelatoAccount || pendingTx
+                          ? 'cursor-not-allowed'
+                          : 'cursor-pointer'
+                      }
+                      text-white
+                      p-4
+                    `}
+                  type='button'
+                  onClick={deposit}
+                  disabled={!gelatoAccount || pendingTx ? true : false}
+                >
+                  Deposit to Gelato Account
+                </button>
+                <button
+                  className={`
+                      ${
+                        !gelatoAccount || pendingTx
+                          ? 'bg-gray-300'
+                          : 'bg-purple-600'
+                      }
+                      ${
+                        !gelatoAccount || pendingTx
+                          ? 'cursor-not-allowed'
+                          : 'cursor-pointer'
+                      }
+                      text-white
+                      p-4
+                    `}
+                  type='button'
+                  onClick={withdraw}
+                  disabled={!gelatoAccount || pendingTx ? true : false}
+                >
+                  Withdraw from Gelato Account
+                </button>
               </div>
-            </>
-          )}
-        {gelatoAccount
-          && (
-            <>
-              <div className="mt-8">
-                <div className="grid grid-cols-2 gap-4 mb-4">
+            </div>
+          </>
+        )}
+        {gelatoAccount && (
+          <>
+            <div className='mt-8'>
+              <div className='grid grid-cols-2 gap-4 mb-4'>
+                <div className=''>
+                  <h3>Buy</h3>
                   <input
-                    defaultValue="0"
-                    min="0"
-                    name="depositAmount"
-                    type="number"
+                    defaultValue='1'
+                    min='1'
+                    max='140'
+                    name='chiTokenAmount'
+                    type='number'
                     ref={register}
                   />
+                  <h3>Chi Token (max 140)</h3>
+                </div>
+              </div>
+              <div className='grid grid-cols-2 gap-4 mb-4'>
+                <div className=''>
+                  <h3>When the GasPrice is lower than</h3>
                   <input
-                    defaultValue="0"
-                    min="0"
-                    name="withdrawAmount"
-                    type="number"
+                    defaultValue={currentGasValue || 0}
+                    min='0'
+                    name='gasThreshold'
+                    type='number'
                     ref={register}
                   />
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                  <button
-                    className={`
-                      ${(!gelatoAccount || pendingTx) ? 'bg-gray-300' : 'bg-purple-600'}
-                      ${(!gelatoAccount || pendingTx) ? 'cursor-not-allowed' : 'cursor-pointer'}
-                      text-white
-                      p-4
-                    `}
-                    type="button"
-                    onClick={deposit}
-                    disabled={(!gelatoAccount || pendingTx) ? true : false}
-                  >
-                    Deposit to Gelato Account
-                  </button>
-                  <button
-                    className={`
-                      ${!gelatoAccount || pendingTx ? 'bg-gray-300' : 'bg-purple-600'}
-                      ${!gelatoAccount || pendingTx ? 'cursor-not-allowed' : 'cursor-pointer'}
-                      text-white
-                      p-4
-                    `}
-                    type="button"
-                    onClick={withdraw}
-                    disabled={(!gelatoAccount || pendingTx) ? true : false}
-                  >
-                    Withdraw from Gelato Account
-                  </button>
+                  <h3>Chi Token</h3>
                 </div>
               </div>
-            </>
-          )}
-        {gelatoAccount
-          && (
-            <>
-              <div className="mt-8">
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                  <div className="">
-                    <h3>Buy</h3>
-                    <input
-                      defaultValue="1"
-                      min="1"
-                      max="140"
-                      name="chiTokenAmount"
-                      type="number"
-                      ref={register}
-                    />
-                    <h3>Chi Token (max 140)</h3>
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                  <div className="">
-                    <h3>When the GasPrice is lower than</h3>
-                    <input
-                      defaultValue={currentGasValue || 0}
-                      min="0"
-                      name="gasThreshold"
-                      type="number"
-                      ref={register}
-                    />
-                    <h3>Chi Token</h3>
-                  </div>
-                </div>
-                <div className="grid grid-cols-2 gap-4">
-                  <button
-                    className={`
-                      ${(!gelatoAccount || pendingTx) ? 'bg-gray-300' : 'bg-purple-600'}
-                      ${(!gelatoAccount || pendingTx) ? 'cursor-not-allowed' : 'cursor-pointer'}
+              <div className='grid grid-cols-2 gap-4'>
+                <button
+                  className={`
+                      ${
+                        !gelatoAccount || pendingTx
+                          ? 'bg-gray-300'
+                          : 'bg-purple-600'
+                      }
+                      ${
+                        !gelatoAccount || pendingTx
+                          ? 'cursor-not-allowed'
+                          : 'cursor-pointer'
+                      }
                       text-white
                       p-4
                     `}
-                    type="button"
-                    onClick={createTaks}
-                    disabled={(!gelatoAccount || pendingTx) ? true : false}
-                  >
-                    Submit a task
-                  </button>
-                </div>
+                  type='button'
+                  onClick={createTaks}
+                  disabled={!gelatoAccount || pendingTx ? true : false}
+                >
+                  Submit a task
+                </button>
               </div>
-              {tasks.length > 0
-                && (
-                  <ShowTasks
-                    data={tasks}
-                    gelatoAccount={gelatoAccount}
-                    getTasks={getTasks}
-                    notify={notify}
-                    pendingTx={pendingTx}
-                    setPendingTx={setPendingTx}
-                    setTasks={setTasks}
-                    userAddress={walletAddress}
-                    web3={web3}
-                  />
-                )}
-            </>
-          )}
+            </div>
+            {tasks.length > 0 && (
+              <ShowTasks
+                data={tasks}
+                gelatoAccount={gelatoAccount}
+                getTasks={getTasks}
+                notify={notify}
+                pendingTx={pendingTx}
+                setPendingTx={setPendingTx}
+                setTasks={setTasks}
+                userAddress={walletAddress}
+                web3={web3}
+              />
+            )}
+          </>
+        )}
       </div>
     </>
   );
